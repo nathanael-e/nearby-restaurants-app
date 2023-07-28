c2399c5cc2fb6dafc9b282b752b2af97a434e02b
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2023-07-28 07:57:49 +0200

    add jest

---
+++ test/app.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react-native';
+import App from '../src/App';
+
+jest.mock(
+    '../src/hooks/useLongitudeLatitude',
+    () => ({
+        useLongitudeLatitude: () => {
+            return {
+                status: 'success',
+                longitude: 123.456,
+                latitude: 78.910,
+            };
+        },
+    })
+);
+
+
+describe('App', () => {
+    it('renders the correct content when location status is success', () => {
+        const { getByText } = render(<App />);
+
+        expect(getByText('longitude: 123.456 latitude: 78.91')).toBeTruthy();
+    });
+});
